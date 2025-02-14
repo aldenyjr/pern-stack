f9c7bcdc9cffa45d146d3864a06555685db7b17f
Aldeny Júnior <aldenyjr.dev@gmail.com>
2025-02-14 15:51:51 -0300

    Database Setup

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,8 @@ import helmet from "helmet";
 import morgan from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
-
 import productRoutes from "./routes/productRoute.js";
+import { sql } from "./config/db.js";
 
 dotenv.config();
 
@@ -24,6 +24,24 @@ app.use(morgan("dev"));
 
 app.use("/api/products", productRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+async function initDB() {
+  try {
+    await sql`
+      CREATE TABLE IF NOT EXISTS products (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        image VARCHAR(255) NOT NULL,
+        price DECIMAL(10, 2) NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      )
+    `;
+    console.log("Database initialized successfully");
+  } catch (error) {
+    console.log("Error initDB", error);
+  }
+}
+initDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 });
