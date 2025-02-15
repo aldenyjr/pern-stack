9d9e0332b56af8fe420a6eb0623eb3cfd4ff5855
Aldeny Júnior <aldenyjr.dev@gmail.com>
2025-02-14 23:57:56 -0300

    implements ProductController

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,5 +1,117 @@
+import { sql } from "../config/db.js";
+
 // Retornar todos os produtos do Banco de Dados
-export const getAllProducts = async (req, res) => {};
+export const getAllProducts = async (req, res) => {
+  try {
+    const products = await sql`
+        SELECT * FROM products
+        ORDER BY created_at DESC
+    `;
+
+    console.log("fetched products", products);
+    res.status(200).json({ success: true, data: products });
+  } catch (error) {
+    console.log("Error in getProducts function", error);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+};
+
+// Retornar um produdo especifico
+export const getProduct = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const product = await sql`
+        SELECT * FROM products
+        WHERE id=${id}
+        ORDER BY created_at DESC
+    `;
+
+    console.log("fetched product", product);
+    res.status(200).json({ success: true, data: product[0] });
+  } catch (error) {
+    console.log("Error in getProduct function", error);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+};
 
 //Criar Produtos
-export const createProduct = async (req, res) => {};
+export const createProduct = async (req, res) => {
+  const { name, price, image } = req.body;
+
+  if (!name || !price || !image) {
+    return res
+      .status(400)
+      .json({ success: false, message: "All fields are required" });
+  }
+
+  try {
+    const newProduct = await sql`
+        INSERT INTO products (name, price, image)
+        VALUES (${name}, ${price}, ${image})
+        RETURNING *
+    
+    `;
+
+    console.log("new Product added", newProduct);
+    res.status(201).json({ success: true, data: newProduct[0] });
+  } catch (error) {
+    console.log("Error in createProducts function", error);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+};
+
+// Atualizar um produto especifico
+export const updateProduct = async (req, res) => {
+  const { id } = req.params;
+  const { name, price, image } = req.body;
+
+  try {
+    const updateProduct = await sql`
+        UPDATE products
+        SET name=${name}, price=${price}, image=${image}
+        WHERE id=${id}
+        RETURNING *
+    `;
+
+    if (updateProduct.length === 0) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Product not found" });
+    }
+
+    console.log("updateProduct added", updateProduct);
+    res.status(200).json({ sucess: true, data: updateProduct[0] });
+  } catch (error) {
+    console.log("Error in updateProduct function", error);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+};
+
+// Deletar um produto especifico
+export const deleteProduct = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const getProduct = await sql`
+        SELECT * FROM products
+        WHERE id=${id}
+    `;
+
+    if (getProduct.length === 0) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Product not found" });
+    }
+
+    const deleteProduct = await sql`
+            DELETE FROM products
+            WHERE id=${id}
+        `;
+
+    res.status(204).json();
+  } catch (error) {
+    console.log("Error in deleteProduct function", error);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ import { sql } from "./config/db.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
