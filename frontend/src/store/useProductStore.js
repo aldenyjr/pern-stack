0644c8dae21f1f75c73df3bd82d17fdb0aebc6ee
Aldeny Júnior <aldenyjr.dev@gmail.com>
2025-02-17 19:54:48 -0300

    Delete Product

--- frontend/src/store/useProductStore.js
+++ frontend/src/store/useProductStore.js
@@ -1,9 +1,10 @@
 import { create } from "zustand";
 import axios from "axios";
+import { toast } from "react-hot-toast";
 
 const BASE_URL = "http://localhost:3000";
 
-export const useProductStore = create((set, get) => ({
+export const useProductStore = create((set) => ({
   products: [],
   loading: false,
   error: null,
@@ -14,8 +15,25 @@ export const useProductStore = create((set, get) => ({
       const response = await axios.get(`${BASE_URL}/api/products`);
       set({ products: response.data.data, error: null });
     } catch (error) {
-      if (error.status == 429) set({ error: "Rate limit exceeded" });
-      else set({ error: "Something went wrong" });
+      if (error.status == 429)
+        set({ error: "Rate limit exceeded", products: [] });
+      else set({ error: "Something went wrong", products: [] });
+    } finally {
+      set({ loading: false });
+    }
+  },
+
+  deleteProduct: async (id) => {
+    set({ loading: true });
+    try {
+      await axios.delete(`${BASE_URL}/api/products/${id}`);
+      set((prev) => ({
+        products: prev.products.filter((product) => product.id !== id),
+      }));
+      toast.success("Product deleted successfully");
+    } catch (error) {
+      console.log("Error in deleteProduct function", error);
+      toast.error("Something went wrong");
     } finally {
       set({ loading: false });
     }
