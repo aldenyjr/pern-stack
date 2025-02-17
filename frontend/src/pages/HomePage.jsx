4ac8037e04ae984636a2583a6120429fda671511
Aldeny Júnior <aldenyjr.dev@gmail.com>
2025-02-17 15:32:45 -0300

    Fetch Products in Home Page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,43 @@
+import { useEffect } from "react";
+import { useProductStore } from "../store/useProductStore";
+import { PlusCircleIcon, RefreshCwIcon } from "lucide-react";
+import ProductCard from "../components/ProductCard";
+
 function HomePage() {
+  const { products, loading, error, fetchProducts } = useProductStore();
+
+  useEffect(() => {
+    fetchProducts();
+  }, [fetchProducts]);
+
   return (
-    <div>
-      <h1>HomePage</h1>
-    </div>
+    <main className="mx-auto px-4 py-8 max-w-6xl">
+      <div className="flex justify-between items-center mb-8">
+        <button className="btn btn-primary">
+          <PlusCircleIcon
+            className="
+          size-5 mr-2"
+          />
+          Add Product
+        </button>
+        <button className="btn btn-primary" onClick={fetchProducts}>
+          <RefreshCwIcon className="size-5" />
+        </button>
+      </div>
+
+      {error && <div className="alert alert-error mb-8">{error}</div>}
+      {loading ? (
+        <div className="flex justify-center items-center">
+          <div className="loading loading-spinner loading-lg"></div>
+        </div>
+      ) : (
+        <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </div>
+      )}
+    </main>
   );
 }
 
